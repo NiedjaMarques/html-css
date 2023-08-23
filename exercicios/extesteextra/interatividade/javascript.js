440222699e626bb11c8c929a9292a8480ebdbed0
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-08-23 10:25:18 -0300

    .

---
+++ exercicios/extesteextra/interatividade/javascript.js
@@ -0,0 +1,6 @@
+function menu(){
+    let menu = document.getElementById('menu') // variavel que chama o meu icone no html
+    let lista = document.getElementById('lista') // variavel que chama minha lista 
+
+    
+}
